11c7228c7b9e9a8c9dfff88edf56439079d69846
Kohoholint <valtorres2022@gmail.com>
2025-01-29 16:02:38 -0500

    Made Repository

---
+++ Assignment1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Xml.Serialization;
+using Assignment1.Models;
+using Library.eCommerce.Services;
+
+namespace MyApp 
+{
+    internal class Program 
+    {
+        static void Main(string[] args) 
+        {
+            var lastKey = 1;
+            Console.WriteLine("Welcome to Amazon!");
+
+            Console.WriteLine("C. Create new inventory item");
+            Console.WriteLine("R. Read all inventory items");
+            Console.WriteLine("u. Update an inventory item");
+            Console.WriteLine("D. Delete an inventory item");
+
+            List<Product?> list = ProductServiceProxy.Current.Products;
+
+            char choice = 'Z';
+            do 
+            {
+                string? input = Console.ReadLine();
+                choice = input[0];
+                switch(choice) 
+                {
+                    case 'C':
+                    case 'c':
+                        list.Add(new Product
+                        {
+                            Id = lastKey++,
+                            Name = Console.ReadLine()
+                        });
+                        break;
+
+                    case 'R':
+                    case 'r':
+                        list.ForEach(Console.WriteLine);
+                        break;
+
+                    case 'U':
+                    case 'u':
+                        //Select one of the products
+                        Console.WriteLine("Which product would you like to update?");
+                        int selection = int.Parse(Console.ReadLine() ?? "-1");
+                        var selectedProd = list.FirstOrDefault(p => p.Id == selection);
+                        if (selectedProd != null)
+                        {
+                            selectedProd.Name = Console.ReadLine() ?? "ERROR";
+                        }
+                        break;
+
+                    case 'D':
+                    case 'd':
+                        //select one of the products
+                        //throw it away
+                        Console.WriteLine("Which product would you like to delete");
+                        selection = int.Parse(Console.ReadLine() ?? "-1");
+                        selectedProd = list.FirstOrDefault(p => p.Id == selection);
+                        list.Remove(selectedProd);
+                        break;
+
+                    default:
+                        Console.WriteLine("Error: Unknown Command");
+                        break;
+                    
+                }
+
+            } while (choice != 'Q' && choice != 'q');
+            Console.ReadLine();
+        }
+        
+
+    }
+}
---
+++ Library.eCommerce/Models/Product.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Display
+        {
+            get
+            {
+                return $"{Id}. {Name}";
+            }
+        }
+
+        public Product()
+        {
+            Name = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return Display ?? string.Empty;
+        }
+    }
+}
