bdc22886da0a8816da0f7560ee54dab213fc267f
That1nerdGoose <valtorres2022@gmail.com>
2025-03-21 11:37:36 -0400

    Adding "Add" functionality to inventory

--- Maui.eCommerce/Views/ProductDetails.xaml.cs
+++ Maui.eCommerce/Views/ProductDetails.xaml.cs
@@ -1,3 +1,4 @@
+using Library.eCommerce.Services;
 using Maui.eCommerce.ViewModels;
 
 namespace Maui.eCommerce.Views;
@@ -13,5 +14,14 @@ public partial class ProductDetails : ContentPage
     private void GoBackClicked(object sender, EventArgs e)
     {
 		Shell.Current.GoToAsync("//InventoryManagement");
+    }
+
+    private void OkClicked(object sender, EventArgs e)
+    {
+        var name = (BindingContext as ProductViewModel)?.Name;
+        decimal price = ((ProductViewModel)BindingContext).Price;
+        int quantity = (int)((ProductViewModel)BindingContext).Quantity;
+        InventoryServiceProxy.Current.AddOrUpdate(new Assignment1.Models.Product { Name = name, Price = price, Quantity = quantity });
+        Shell.Current.GoToAsync("//InventoryManagement");
     }
 }
